592d2511ec2d1d99574abf21b62d998a824ca754
Sharifia <el.sh200@gmail.com>
2025-01-25 21:02:36 +0330

    Argument

--- main.py
+++ main.py
@@ -5,7 +5,8 @@ import click
 @click.option('--name', '-n', default='Elham', help='Firstname description')
 # @click.option('--name', '-n', nargs=2)
 @click.option('--location', '-l', multiple=True)
-def main(name, location):
+@click.argument('age', default=23, type=int)
+def main(name, location, age):
     click.echo(f'Hello, {name}!')
     click.echo(f'\n{location}')
 
